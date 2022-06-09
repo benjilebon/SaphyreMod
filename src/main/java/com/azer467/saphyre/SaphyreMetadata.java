9f0c777fc8f2d76e85eac8d7624abdc07896fd01
Benjamin Lebon <benji.lebon467@gmail.com>
2022-06-09 23:28:21 +0200

    Advancements + Code cleanup
    Signed-off-by: Benjamin Lebon <benji.lebon467@gmail.com>

--- src/main/java/com/azer467/saphyre/SaphyreMetadata.java
+++ src/main/java/com/azer467/saphyre/SaphyreMetadata.java
@@ -3,5 +3,4 @@ package com.azer467.saphyre;
 public class SaphyreMetadata {
     public static final String MODID = "saphyre";
     public static final String NAME = "Saphyre";
-    public static final String VERSION = "0.3";
 }
--- src/main/java/com/azer467/saphyre/network/NetworkPacketsInstance.java
+++
@@ -1,8 +0,0 @@
-//package com.azer467.saphyre.network;
-//
-//import net.minecraftforge.fml.common.network.NetworkRegistry;
-//import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-//
-//public class NetworkPacketsInstance {
-//    public static final SimpleNetworkWrapper NETWORK_SERVER_INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("saphyre");
-//}
