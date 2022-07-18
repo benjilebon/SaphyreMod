8eaab0e3eab3852f24a2daa229296415d8805e8b
Benjamin Lebon <benji.lebon467@gmail.com>
2022-07-18 11:22:15 +0200

    Release 1.0.2
    Signed-off-by: Benjamin Lebon <benji.lebon467@gmail.com>

--- src/main/java/com/benjilebon/saphyre/events/particles/ParticleEvents.java
+++ src/main/java/com/benjilebon/saphyre/events/particles/ParticleEvents.java
@@ -3,19 +3,18 @@ package com.benjilebon.saphyre.events.particles;
 import com.benjilebon.saphyre.SaphyreMetadata;
 import com.benjilebon.saphyre.particles.GiftParticle;
 import com.benjilebon.saphyre.init.ParticlesInit;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = SaphyreMetadata.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParticleEvents {
     @SubscribeEvent
-    public static void registerParticleFactories(final ParticleFactoryRegisterEvent event) {
-        Minecraft.getInstance().particleEngine.register(ParticlesInit.GIFT_PARTICLES.get(), GiftParticle.GiftProvider::new);
-        Minecraft.getInstance().particleEngine.register(ParticlesInit.ENHANCED_GIFT_PARTICLES.get(), GiftParticle.EnhancedGiftProvider::new);
-        Minecraft.getInstance().particleEngine.register(ParticlesInit.AMAZING_GIFT_PARTICLES.get(), GiftParticle.AmazingGiftProvider::new);
-        Minecraft.getInstance().particleEngine.register(ParticlesInit.INCREDIBLE_GIFT_PARTICLES.get(), GiftParticle.IncredibleGiftProvider::new);
-        Minecraft.getInstance().particleEngine.register(ParticlesInit.ULTIMATE_GIFT_PARTICLES.get(), GiftParticle.UltimateGiftProvider::new);
+    public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
+       event.register(ParticlesInit.GIFT_PARTICLES.get(), GiftParticle.GiftProvider::new);
+       event.register(ParticlesInit.ENHANCED_GIFT_PARTICLES.get(), GiftParticle.EnhancedGiftProvider::new);
+       event.register(ParticlesInit.AMAZING_GIFT_PARTICLES.get(), GiftParticle.AmazingGiftProvider::new);
+       event.register(ParticlesInit.INCREDIBLE_GIFT_PARTICLES.get(), GiftParticle.IncredibleGiftProvider::new);
+       event.register(ParticlesInit.ULTIMATE_GIFT_PARTICLES.get(), GiftParticle.UltimateGiftProvider::new);
     }
 }
